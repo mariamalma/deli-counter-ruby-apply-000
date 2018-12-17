1b92be34f088224a226aaad61c5b97fcbfd6f4fd
Mariam Ibrahim <mibrah7@students.towson.edu>
2018-12-17 01:37:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,13 @@
-# Write your code here.
-def take_a_number
-katz_deli=["Ada","Grace","Kent"]
-line=[1, 2, 3, 4]
-puts "Welcome, #{name},+ you are number #{line+1} in line."
+def katz_deli 
+  puts 
 end 
 
-take_a_number(katz_deli, "Ada") #=> "Currently serving"
-take_a_number(katz_deli, "Grace")#=>"Currenly serving"
-take_a_number(katz_deli, "Kent")#=>"Currently serving"
-
-line(katz_deli)#=> "The line is currently: 1. Ada 2. Grace 3. Kent"
-
-
-when  line<1 
-  puts "The line is currently empty!"
-when line=1 
-  puts "Ada"
-when line=2 
-  puts "Grace"
-when line=3 
-  puts"Kent"
+def take_a_number ("katz_deli","name")
+  puts
 end
 
+now_serving 
+puts
 
-def now_serving
+  
+  
